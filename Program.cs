c842890fd59502137b07e7640d652441f59724fb
SAM SOUZA <samuelluizsouza771@gmail.com>
2025-06-28 00:39:44 -0300

    adicionei check e delete das tarefas

--- Program.cs
+++ Program.cs
@@ -34,11 +34,42 @@ class Program
             Console.WriteLine(e.Message);
         }
 
-        AddTask(tarefas, pathtsk);
+        Menu(tarefas, pathtsk);
+    }
+    static void Menu(List<tarefa> tarefas, string path){
+        var choice = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .Title("Choice:")
+                .PageSize(10)
+                .MoreChoicesText("[grey](Use ↑ ↓ para navegar)[/]")
+                .AddChoices(new[]{
+                    "Add Task", "List Tasks", "Check Tasks","Delete Tasks", "Exit"
+                })
+        );
+
+        switch (choice){
+            case "Add Task":
+                AddTask(tarefas, path);
+            break;
+            case "List Tasks":
+                ListTasks(tarefas, path);
+            break;
+            case "Check Tasks":
+                var chkselected = SelectTasks(tarefas);
+                CheckTasks(chkselected, tarefas, path);
+            break;
+            case "Delete Tasks":
+                var delselected = SelectTasks(tarefas);
+                DeleteTasks(delselected, tarefas, path);
+            break;
+            case "Exit":
+                Environment.Exit(0);
+            break;
+        }
     }
     
     static void AddTask(List<tarefa> tarefas, string path) {
-        string titulo = AnsiConsole.Ask<string>("Digite o título: ");
+        string titulo = AnsiConsole.Ask<string>("Type the task title: ");
         int novoId = tarefas.Any() ? tarefas.Max(t => t.Id) + 1 : 1;
 
         tarefa novaTarefa = new() {
@@ -49,8 +80,7 @@ class Program
 
         tarefas.Add(novaTarefa);
         SaveTask(tarefas, path);
-        SelectTasks(tarefas);
-        ListTasks(tarefas);
+        ListTasks(tarefas, path);
     }
     
     static void SaveTask(List<tarefa> tarefas, string path){
@@ -58,7 +88,7 @@ class Program
         File.WriteAllText(path, json);
     }
 
-    static void ListTasks(List<tarefa> tarefas){
+    static void ListTasks(List<tarefa> tarefas, string path){
         var table = new Table();
         table.AddColumn("ID");
         table.AddColumn("Tarefa");
@@ -68,12 +98,14 @@ class Program
             string status = t.Status ? "[green]Feita[/]" : "[red]Pendente[/]";
             table.AddRow(t.Id.ToString(), t.Name, status);
         }
-
+        Console.Clear();
         AnsiConsole.Write(table);
+        SaveTask(tarefas, path);
+        Menu(tarefas, path);
     }
 
-    static void SelectTasks(List<tarefa> tarefas){
-        AnsiConsole.Prompt(
+    static List<string> SelectTasks(List<tarefa> tarefas){
+        var selected = AnsiConsole.Prompt(
             new MultiSelectionPrompt<string>()
                 .Title("Quais tarefas deseja marcar como feito? ")
                 .NotRequired()
@@ -82,6 +114,30 @@ class Program
                 .InstructionsText("[grey](Pressione Espaço para marcar e Enter para confirmar)[/]")
                 .AddChoices(tarefas.Select(t => $"{t.Id} - {t.Name}"))
         );
+
+        return selected;
+    }
+
+    static void CheckTasks(List<string> selected, List<tarefa> tarefas, string path){
+        foreach(var item in selected){
+            var partes = item.Split(" - ");
+            int id = int.Parse(partes[0]);
+
+            var tarefaEncontrada =tarefas.Find(t => t.Id == id);
+            tarefaEncontrada.Status = true;
+        }
+        ListTasks(tarefas, path);
+    }
+
+    static void DeleteTasks(List<string> selected, List<tarefa> tarefas, string path){
+        foreach(var item in selected){
+            var partes = item.Split(" - ");
+            int id = int.Parse(partes[0]);
+
+            var tarefaEncontrada =tarefas.Find(t => t.Id == id);
+            tarefas.Remove(tarefaEncontrada);
+        }
+        ListTasks(tarefas, path);
     }
     
 }
